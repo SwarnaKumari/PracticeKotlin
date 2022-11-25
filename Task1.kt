0355d5208c35e0ec6d81f666a9fed2a97dcd3398
SwarnaKumari <50829003+SwarnaKumari@users.noreply.github.com>
2022-11-25 12:12:12 +0530

    Update Task1.kt

--- Task1.kt
+++ Task1.kt
@@ -1,8 +1,11 @@
 fun main(){
     println("Numbers divisible by 3 and 5 are:")
+    var count=0
     for(i in 1..100){
         if(i%3==0 && i%5==0){
+            count+=1
             println(i)
         }
     }
+    println("$count numbers are divisible by 3 and 5")
 }
